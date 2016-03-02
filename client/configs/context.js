25ec665b6595e2eee7d6556ddf4227ea634353f6
Nolan Darilek <nolan@thewordnerd.info>
2016-03-01 20:57:22 -0600

    rm

--- client/configs/context.js
+++ client/configs/context.js
@@ -1,13 +1,11 @@
 import {FlowRouter} from "meteor/kadira:flow-router"
 import {Accounts} from "meteor/accounts-base"
 import {Meteor} from "meteor/meteor"
-import {SubsManager} from "meteor/meteorhacks:subs-manager"
 import {Tracker} from "meteor/tracker"
 
 export default () => ({
   Accounts,
   FlowRouter,
   Meteor,
-  Subscriptions: new SubsManager(),
   Tracker
 })
