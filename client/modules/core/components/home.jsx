d851ed278e38094938023ed83c32125514c4a5a8
Nolan Darilek <nolan@thewordnerd.info>
2016-03-01 20:26:09 -0600

    Remove lib.

--- client/modules/core/components/home.jsx
+++ client/modules/core/components/home.jsx
@@ -4,6 +4,5 @@ import React from "react"
 import toastr from "toastr"
 
 import {Landing} from "/client/modules/landing"
-import {Accessible} from "/lib/client/ui"
 
-export default Accessible(({userId}) => <p>Welcome home.</p>)
+export default ({userId}) => <p>Welcome home.</p>
