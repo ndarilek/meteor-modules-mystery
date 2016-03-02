4accccdb115b70d2a07b974cdcfe59dcdb53afbf
Nolan Darilek <nolan@thewordnerd.info>
2016-03-01 21:07:31 -0600

    rm

--- client/modules/core/components/main_layout.jsx
+++ client/modules/core/components/main_layout.jsx
@@ -1,30 +1,3 @@
 import React from "react"
-import {Col, Grid, Nav, Navbar, NavItem, Row} from "react-bootstrap"
 
-export default ({main}) => <div>
-  <Grid fluid={true}>
-    <Row>
-      <Col md={12}>
-        <main>
-          {main()}
-        </main>
-      </Col>
-    </Row>
-  </Grid>
-  <footer className="footer">
-    <Grid fluid={true}>
-      <Row>
-        <Col md={8}>
-          <Navbar>
-            <NavItem href="/blog">Blog</NavItem>
-            <NavItem href={FlowRouter.path("terms")}>Terms of Use</NavItem>
-            <NavItem href={FlowRouter.path("privacy")}>Privacy Policy</NavItem>
-          </Navbar>
-        </Col>
-        <Col md={4}>
-          <p className="text-right">Copyright &copy; 2015-2016 Nolan Darilek.</p>
-        </Col>
-      </Row>
-    </Grid>
-  </footer>
-</div>
+export default ({main}) => <div>{main()}</div>
