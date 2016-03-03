063cc353f5685e4d1b1236a4505a0d622637a407
Nolan Darilek <nolan@thewordnerd.info>
2016-03-03 12:45:47 -0600

    rm

--- client/main.js
+++ client/main.js
@@ -1,9 +1,7 @@
 import {createApp} from "mantra-core"
 
 import coreModule from "./modules/core"
-import landing from "./modules/landing"
 
 const app = createApp({})
 app.loadModule(coreModule)
-app.loadModule(landing)
 app.init()
