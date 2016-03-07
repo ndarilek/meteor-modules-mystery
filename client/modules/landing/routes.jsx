a26063ac1c64c996636aecee4e07bb58ff466933
Nolan Darilek <nolan@thewordnerd.info>
2016-03-07 13:24:44 -0600

    Have to import this component to trigger the error on uncomment of other import.

--- client/modules/landing/routes.jsx
+++ client/modules/landing/routes.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import {mount} from "react-mounter"
 
-//import {MainLayout} from "/client/modules/core"
+import {MainLayout} from "/client/modules/core"
 
 export default {}
